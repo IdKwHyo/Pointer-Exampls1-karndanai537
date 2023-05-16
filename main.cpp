1c2d510546c41279331c7d3263a2d24bc2604a1c
AjMing <62439174+AjMing@users.noreply.github.com>
2023-05-16 08:40:01 +0000

    my new code

--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
+#include <iomanip>
+#define SIZE 10
+
 using namespace std;
 
 int main(){
-
-  int a;
+  /*int a;
   double d;
   cout<<&a<<" size "<<sizeof(a) <<endl;
   cout<<&d<<" size "<<sizeof(d) <<endl; 
-  
-  /*  int x,*p;
-    p=&x;
-    x=5;
+  */
+  /*
+   
 
     cout<< x  <<" "<< *p <<endl;
     cout<< &x <<" "<<  p <<endl;
-    cout<< &p <<endl;*/
-
+    cout<< &p <<endl;
+   int b=9;
+   cout<< x  <<" "<< b << " "<< *p <<endl;
+    x++;
+   cout<< x  <<" "<< b << " "<< *p <<endl;
+    p=&b; *p=2;
+  cout<< x  <<" "<< b << " "<< *p <<endl;	
+  
+   */
 /*Example Array*/
   /*
 int *ip;
@@ -37,4 +45,17 @@ cout<<ip<<" "<<*ip<<endl; 	// print
  /*the address is 4324, not 4321
  because the pointer is incremented 						
  by the size of an integer (4 bytes) */
+
+  char *pa, *pb, i, temp;
+char a[SIZE] ;
+  cout<<"Input string:";
+  cin>>a;
+  
+pa = &a[0]; //pa =a;
+cout<<"Original: ";
+for (i=0; i<SIZE-1; i++ ,pa++){
+   cout<<setw(3)<<*pa ;//<<endl;
+  //cout<<pa<<endl;
+}
+cout<<setw(3)<<*pa<<endl;
 }
